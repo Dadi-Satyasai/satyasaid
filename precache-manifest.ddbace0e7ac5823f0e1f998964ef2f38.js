95676cf79c8b468a17e0c654459510ae5975af4b
Dadi-Satyasai <satyasai106dadi@gmail.com>
2022-05-24 18:40:09 +0530

    Updates

---
+++ precache-manifest.ddbace0e7ac5823f0e1f998964ef2f38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc2d07cba11f948758c4",
+    "url": "/satyasaid/static/css/main.1427a62c.chunk.css"
+  },
+  {
+    "revision": "dc2d07cba11f948758c4",
+    "url": "/satyasaid/static/js/main.dc2d07cb.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/satyasaid/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "881d40ac1236a97b7469",
+    "url": "/satyasaid/static/js/2.881d40ac.chunk.js"
+  },
+  {
+    "revision": "1265af0036d4110ba0e2",
+    "url": "/satyasaid/static/js/runtime~main.1265af00.js"
+  },
+  {
+    "revision": "edb4f5c0b6de749bcabb26549d01e702",
+    "url": "/satyasaid/index.html"
+  }
+];
